1830d1db1348c55c4055b912d65ad17a38ef5d49
Yryskeldi Anarbekov <sophiessaphilipp@gmail.com>
2023-10-23 11:33:32 -0500

    changed db to asynchronous

--- sql_db.py
+++ sql_db.py
@@ -1,45 +1,37 @@
-#general imports 
-import sqlite3
+import aiosqlite
 
-
-def start_database():
-    global base, cur
-
-    base = sqlite3.connect('main.db')
-    cur  = base.cursor()
-
-    if base:
-        print('connected to the main.db.......')
-        
-    #TODO: change to user_id, instead of using chat_id
-
-    #a table for users
-    base.execute('CREATE TABLE IF NOT EXISTS users(chat_id TEXT UNIQUE, username TEXT, language TEXT)')
-    #a table for coin entries of users, consists of the chat_id (might change to user_id in the future), coin name and the amount of coins
-    base.execute('CREATE TABLE IF NOT EXISTS wallet(chat_id TEXT, coin_code TEXT, amount INTEGER, )')
-
+async def start_database():
+    async with aiosqlite.connect('main.db') as db:
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                chat_id TEXT UNIQUE,
+                username TEXT,
+                language TEXT
+            )
+        """)
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS wallet(
+                user_id INTEGER,
+                chat_id TEXT,
+                coin_code TEXT,
+                amount INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users(user_id)
+            )
+        """)
+        print('Connected to the main.db and ensured tables exist...')
 
 async def add_user(chat_id, username, language):
-    await cur.execute(f'INSERT OR IGNORE INTO users VALUES ({chat_id}, {username}, {language})')
-    base.commit()
+    async with aiosqlite.connect('main.db') as db:
+        await db.execute('INSERT INTO users (chat_id, username, language) VALUES (?, ?, ?)', (chat_id, username, language))
+        await db.commit()
 
 
 async def get_lang(chat_id):
-    await cur.execute(f'SELECT lang FROM users WHERE chat_id = {chat_id}')
-
-async def add_coin(state, id, lang):
-    async with state.proxy() as data:
-        cur.execute(f"INSERT INTO balances VALUES ({id}, ?, ?, {lang})", tuple(data.values()))
-        base.commit()
-
-async def subract_coin(state, id):
-    #TODO: subract certain amount from the balance of a certain coin
-    pass
-
-async def del_coin(id: str, coin_code: str):
-    cur.execute(f"DELETE FROM balances WHERE id = {id} AND coin_code = '{coin_code}'")
-    base.commit()
-
-async def get_coins(id):
-    return cur.execute(f'SELECT * FROM balances WHERE id == {id}')
-
+    async with aiosqlite.connect('main.db') as db:
+        cursor = await db.execute('SELECT language FROM users WHERE chat_id = ?', (chat_id))
+        row = await cursor.fetchone()
+        if row:
+            return row[0]
+        else:
+            return None
